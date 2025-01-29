ad2f71ea8db8675289c4d238a1a3045ce69929b3
Julczan <julek111222h@gmail.com>
2025-01-29 01:53:12 +0100

    06 is almost done, the fix note is written

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  //Can be devided by four
+  //Can't be devided by 100 unless they are divisable by 400 as well
 
+  //To fix: 1900 is divisable by 4
+  if (year % 4 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
