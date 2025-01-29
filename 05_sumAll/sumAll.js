e3a578fec814471e1e8e1635b29c3bea32249b9e
Julczan <julek111222h@gmail.com>
2025-01-29 01:24:36 +0100

    fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, lastNum) {
+  let finalSum = 0;
+  if (typeof firstNum !== "number" || typeof lastNum !== "number") {
+    return "ERROR";
+  } else if (firstNum < 0 || lastNum < 0) {
+    return "ERROR";
+  } else if (
+    Number.isInteger(firstNum) === false ||
+    Number.isInteger(lastNum) === false
+  ) {
+    return "ERROR";
+  } else if (firstNum > lastNum) {
+    for (let i = firstNum; i >= lastNum; i--) {
+      finalSum += i;
+    }
+  } else {
+    for (let i = firstNum; i <= lastNum; i++) {
+      finalSum += i;
+    }
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
